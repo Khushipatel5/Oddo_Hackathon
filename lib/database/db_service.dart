e22ff0488c98592c3dbac93d0a8b478230201d59
Dhruvi Valera <dhruvivalera31@gmail.com>
2025-07-12 10:28:29 +0530

    COMMITED BY DHRUVI VALERA

--- lib/database/db_service.dart
+++ lib/database/db_service.dart
@@ -65,9 +65,9 @@ class AppDatabase{
             FOREIGN KEY ($COL_REQUESTED_SKILL_ID) REFERENCES $TBL_SKILLS_NAME($COL_SKILL_ID)
           )
         ''');
-
-        return _database!;
       },
     );
+
+    return _database!;
   }
 }
